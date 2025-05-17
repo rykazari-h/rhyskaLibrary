8df8ad5ec9ac96cb4f5129071e441e96354ef888
rykazari <ryota.books@outlook.jp>
2025-05-18 02:55:24 +0900

    added bit

---
+++ ftree.h
@@ -0,0 +1,15 @@
+#include <vector>
+template<class T>struct ftree{
+public:
+	explicit ftree(int _n=0):n(_n),d(_n+1){}
+	void add(int i,T x){for(i++;i<=n;i+=i&-i)d[i]+=x;}
+	T sum(int l, int r){return sum(r-1)-sum(l-1);}
+	T sum(int i){
+		T s=0;
+		for(i++;i;i-=i&-i)s+=d[i];
+		return s;
+	}
+private:
+	int n;
+	std::vector<T> d;
+};
