ce89e99f480fb8987e165d015dc9cf7191bdd649
rykazari <ryota.books@outlook.jp>
2025-05-19 03:51:03 +0900

    added function to cc

--- cc.h
+++ cc.h
@@ -3,15 +3,31 @@
 template<class T=int>class cc{
 public:
 	cc():did(0){}
+	void clear(){did=0;d.clear();}
+
+	//要素の追加
 	void push(T x){d.push_back(x),did=0;}
+	void push(const std::vector<T> &v){for(const T &x:v)d.push_back(x);did=0;}
+
+	//実座標->index
 	int operator()(T x){
 		init();
 		return std::upper_bound(d.begin(),d.end(),x)-d.begin()-1;
 	}
+	std::vector<int> press(const std::vector<T> &v){
+		init();
+		std::vector<int>res;
+		for(const T &x:v)res.push_back(std::upper_bound(d.begin(),d.end(),x)-d.begin()-1);
+		return res;
+	}
+
+	//index->実座標
 	T operator[](int x){
 		init();
 		return d[x];
 	}
+
+	//set(array).size
 	int size(){
 		init();
 		return d.size();
