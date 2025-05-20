e25284fa29b88140fde05a575439b6fb11537566
rykazari <ryota.books@outlook.jp>
2025-05-20 21:05:28 +0900

    コンストラクタ、push関数no

--- cc.h
+++ cc.h
@@ -3,38 +3,40 @@
 template<class T=int>class cc{
 public:
 	cc():did(0){}
+	explicit cc(std::vector<T> &v):did(0),d(v){}
+	explicit cc(T *x,T *end):did(0){push(x,end);}
 	void clear(){did=0;d.clear();}
 
 	//要素の追加
 	void push(T x){d.push_back(x),did=0;}
-	void push(const std::vector<T> &v){for(const T &x:v)d.push_back(x);did=0;}
+	void push(const std::vector<T> &v){for(const T &x:v)push(x);}
+	void push(T *x,T *end){for(;x!=end;x++)push(*x);}
 
 	//実座標->index
 	int operator()(T x){
-		init();
+		if(!did)init();
 		return std::upper_bound(d.begin(),d.end(),x)-d.begin()-1;
 	}
 	std::vector<int> press(const std::vector<T> &v){
-		init();
+		if(!did)init();
 		std::vector<int>res;
-		for(const T &x:v)res.push_back(std::upper_bound(d.begin(),d.end(),x)-d.begin()-1);
+		for(const T &x:v)res.push_back(*this(x));
 		return res;
 	}
 
 	//index->実座標
 	T operator[](int x){
-		init();
+		if(!did)init();
 		return d[x];
 	}
 
 	//set(array).size
 	int size(){
-		init();
+		if(!did)init();
 		return d.size();
 	}
 private:
 	void init(){
-		if(did)return;
 		std::sort(d.begin(),d.end());
 		d.erase(std::unique(d.begin(),d.end()),d.end());
 		did=1;
