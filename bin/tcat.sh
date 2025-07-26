3afea60745ccbe45ed9e0b0528922debeb9e0091
rykazari <ryota.books@outlook.jp>
2025-07-27 00:46:19 +0900

    バグ修正

--- bin/tcat.sh
+++ bin/tcat.sh
@@ -1,50 +1,44 @@
-#!/bin/sh
-
+#!/usr/bin/bash
 INCLUDED_FILES=""
-
 expand() {
-	file="$1"
-	abs_path=$(realpath "$file")
-
-	# guard
-	if echo "$INCLUDED_FILES" | grep -qFx "$abs_path"; then
-		return
-	fi
-	INCLUDED_FILES="$INCLUDED_FILES
+    file="$1"
+    abs_path=$(realpath "$file")
+    case "$INCLUDED_FILES" in
+        *"$abs_path"*) return ;;
+    esac
+    INCLUDED_FILES="$INCLUDED_FILES
 $abs_path"
-
-	while IFS= read -r line; do
-		case "$line" in
-			\#include[\ \	]*\<bits/*\>|\
-\#include[\ \	]*\<atcoder/*\>|\
-\#include[\ \	]*\<all.h\>)
-				# bits/ と atcoder/ はスルー
-				echo "$line"
-				;;
-			\#include[\ \	]*\<*\>) 
-				# ./libから
-				inc_path=$(echo "$line" | sed -n 's/#include[[:space:]]*<\([^>]*\)>/\1/p')
-				real_path="./lib/src/$inc_path"
-				if [ -f "$real_path" ]; then
-					expand "$real_path"
-				else
-					echo "$line"
-				fi
-				;;
-			\#pragma[\ \	]*once)
-				# #pragma once は出力しない
-				;;
-			*)
-				echo "$line"
-				;;
-		esac
-	done < "$file"
+    while IFS= read -r line; do
+        case "$line" in
+            \#include\<bits/*\>        | \
+            \#include\<atcoder/*\>     | \
+            \#include\<all.h\>         | \
+            \#include[[:space:]]\<bits/*\>    | \
+            \#include[[:space:]]\<atcoder/*\> | \
+            \#include[[:space:]]\<all.h\>)
+                echo "$line"
+                ;;
+            \#include[[:space:]]\<*[\>] | \
+            \#include\<*[\>])
+                tmp="${line#*<}"
+                inc_path="${tmp%%>*}"
+                real_path="./lib/src/$inc_path"
+                if [ -f "$real_path" ]; then
+                    expand "$real_path"
+                else
+                    echo "$line"
+                fi
+                ;;
+            \#pragma[[:space:]]once)
+                ;;
+            *)
+                echo "$line"
+                ;;
+        esac
+    done < "$file"
 }
-
 if [ $# -ne 1 ]; then
-	echo "Usage: $0 filename" >&2
-	exit 1
+    echo "Usage: $0 filename" >&2
+    exit 1
 fi
-
 expand "$1"
-
