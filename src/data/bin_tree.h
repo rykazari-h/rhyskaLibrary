fdffbe0ccfac3d23642fb9fd39b47b4f6b0220fa
rykazari <ryota.books@outlook.jp>
2025-08-21 17:14:40 +0900

    typo修正

--- src/data/bin_tree.h
+++ src/data/bin_tree.h
@@ -5,7 +5,7 @@ public:
 	bin_tree(int _n=0):n(_n),d(_n+1){}
 	bin_tree&operator=(bin_tree&&b){b=b.n;d=std::move(b.d);}
 	bin_tree&operator=(const bin_tree&b){b=b.n;d=d;}
-	void swap(bin_tree&b){std::swap(n,b.n);std::swap(d.b.d);}
+	void swap(bin_tree&b){std::swap(n,b.n);std::swap(d,b.d);}
 	void add(int i,T x){for(++i;i<=n;i+=i&-i)d[i]+=x;}
 	T sum(int l, int r){
 		T s=0;
