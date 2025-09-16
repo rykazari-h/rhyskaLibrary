4f1d7eccb2287c365c8f294398a2d7d18f753137
rykazari <ryota.books@outlook.jp>
2025-09-16 20:27:48 +0900

    cc.h

---
+++ src/algo/cc.h
@@ -0,0 +1,39 @@
+#include <vector>
+#include <algorithm>
+template<class T=int>class cc{
+public:
+	cc():did(0){}
+	explicit cc(std::vector<T> &v):did(0),d(v){}
+	explicit cc(T *x,T *end):did(0){push(x,end);}
+	void clear(){did=0;d.clear();}
+	void push(T x){d.push_back(x),did=0;}
+	void push(const std::vector<T> &v){for(const T &x:v)push(x);}
+	void push(T *x,T *end){for(;x!=end;x++)push(*x);}
+	int operator()(T x){
+		if(!did)init();
+		return std::upper_bound(d.begin(),d.end(),x)-d.begin()-1;
+	}
+	template<class S>
+	std::vector<int> press(S x,S end){
+		if(!did)init();
+		std::vector<int>res;
+		for(;x!=end;x++)res.push_back((*this)(*x));
+		return res;
+	}
+	T operator[](int x){
+		if(!did)init();
+		return d[x];
+	}
+	int size(){
+		if(!did)init();
+		return d.size();
+	}
+private:
+	void init(){
+		std::sort(d.begin(),d.end());
+		d.erase(std::unique(d.begin(),d.end()),d.end());
+		did=1;
+	}
+	int did;
+	std::vector<T>d;
+};
