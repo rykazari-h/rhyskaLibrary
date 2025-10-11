473cb0aec89baf394e2175bba0a2933b7ade7d6d
rykazari <ryota.books@outlook.jp>
2025-10-11 15:35:41 +0900

    整形

--- crystal/src/header.cr
+++ crystal/src/header.cr
@@ -2,7 +2,7 @@ struct Bool;def to_i;self ? 1 : 0;end;end
 macro vgen(*v);Array.new({{v[0]}}){ {% if v.size==2 %}{{v[1]}}{% else %}vgen({{*v[1..]}}){% end %} };end
 macro rvgen(type,*v);{% if v.size==2 %}Array({{type}}).new({{v[0]}}){{{v[1]}}}{% else %}Array.new({{v[0]}}){rvgen({{type}},{{*v[1..]}})}{% end %};end
 def putv(a : Array(T)) forall T;return if a.empty?;print a[0];(1...a.size).each{|i|putv " ";putv a[i]};end
-def putv(a : Array(Array(T))) forall T;return if a.empty?;putv a[0];a[1..].each{|x|putv "\n";putv x};end
+def putv(a : Array(Array(T))) forall T;return if a.empty?;putv a[0];(1...a.size).each{|i|puts;putv a[i]};end
 def putv(a : Tuple(*T)) forall T;return if a.empty?;putv a[0];(1...a.size).each{|i|putv " ";putv a[i]};end
 def putv(a);print a;end
 def outl;puts;end
