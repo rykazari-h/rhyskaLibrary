9e1b6048534feee2a8659d65a4f6bdda650b1e78
rykazari <ryota.books@outlook.jp>
2025-10-11 14:45:37 +0900

    header <- vgen,rvgen

--- crystal/src/header.cr
+++ crystal/src/header.cr
@@ -1,5 +1,15 @@
 struct Bool
-	def to_i
-		self ? 1 : 0
-	end
+    def to_i
+        self ? 1 : 0
+    end
+end
+macro vgen(*v)
+    Array.new({{v[0]}}){ {% if v.size==2 %}{{v[1]}}{% else %}vgen({{*v[1..]}}){% end %} }
+end
+macro rvgen(type, *v)
+    {% if v.size==2 %}
+        Array({{type}}).new({{v[0]}}){{{v[1]}}}
+    {% else %}
+        Array.new({{v[0]}}){rvgen({{type}},{{*v[1..]}})}
+    {% end %}
 end
