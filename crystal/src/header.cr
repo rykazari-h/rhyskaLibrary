c404c58775992b7faa52fae4f060160369be7566
rykazari <ryota.books@outlook.jp>
2025-10-11 14:50:48 +0900

    短くした

--- crystal/src/header.cr
+++ crystal/src/header.cr
@@ -3,13 +3,5 @@ struct Bool
         self ? 1 : 0
     end
 end
-macro vgen(*v)
-    Array.new({{v[0]}}){ {% if v.size==2 %}{{v[1]}}{% else %}vgen({{*v[1..]}}){% end %} }
-end
-macro rvgen(type, *v)
-    {% if v.size==2 %}
-        Array({{type}}).new({{v[0]}}){{{v[1]}}}
-    {% else %}
-        Array.new({{v[0]}}){rvgen({{type}},{{*v[1..]}})}
-    {% end %}
-end
+macro vgen(*v);Array.new({{v[0]}}){ {% if v.size==2 %}{{v[1]}}{% else %}vgen({{*v[1..]}}){% end %} };end
+macro rvgen(type,*v);{% if v.size==2 %}Array({{type}}).new({{v[0]}}){{{v[1]}}}{% else %}Array.new({{v[0]}}){rvgen({{type}},{{*v[1..]}})}{% end %};end
