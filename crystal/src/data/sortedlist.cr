a9b46e024331a0b7a4bd96ee3ef2ab653f2d5cf4
rykazari <ryota.books@outlook.jp>
2025-10-15 17:56:19 +0900

    multiset修正

--- crystal/src/data/sortedlist.cr
+++ crystal/src/data/sortedlist.cr
@@ -7,11 +7,12 @@ class Sortedlist(T)
 		@size = z.size
 		s = Math.sqrt(@size.to_f/RATIO).ceil.to_i
 		@list = Array(Array(T)).new(s) { [] of T }
-		s.times { |i| @list[i] = z[(@size*i//s)...(@size*(i + 1)//s)] }
+		s.times { |i| @list[i] = z[(@size*i// s)...(@size*(i + 1)// s)] }
 	end
 	def empty?;@size == 0;end
 	def size;@size;end
 	def clear;@size = 0; @list.clear;end
+	def clone;Sortedlist(T).new(@size,@list);end
 	def _position(x : T)
 		sz = @list.size
 		sz.times { |i| return {i, @list[i].bsearch_index { |v| v >= x }.not_nil!} if x <= @list[i][-1] }
@@ -49,7 +50,7 @@ class Sortedlist(T)
 		return false if i == @list[bi].size || @list[bi][i] != x
 		while true
 			@size -= @list[bi].count(x)
-			@list[bi].delete_if { |v| v == x }
+			@list[bi].delete(x)
 			if @list[bi].empty?
 				@list.delete_at(bi)
 			else
@@ -73,17 +74,6 @@ class Sortedlist(T)
 		end
 		@list[-1][-1]
 	end
-	def merge(b : Sortedlist(T))
-		if self.size < b.size
-			temp_list = @list
-			@list = b.list
-			b.list = temp_list
-			temp_size = @size
-			@size = b.size
-			b.size = temp_size
-		end
-		b.@list.each { |v| v.each { |x| insert(x) } }
-	end
 	def count(x : T);index_rg(x) - index(x);end
 	def find(x : T)
 		return false if empty?
@@ -105,7 +95,26 @@ class Sortedlist(T)
 		end
 		@list[-1][-1]
 	end
+	def []=(i : Int32, x : T)
+		if i >= 0
+			@list.size.times do |b|
+				return @list[b][i] = x if i < @list[b].size
+				i -= @list[b].size
+			end
+		else
+			(@list.size - 1).downto(0) do |b|
+				i += @list[b].size
+				return @list[b][i] = x if i >= 0
+			end
+		end
+		@list[-1][-1]
+	end
 	def a;@list;end
+	def to_a
+		r=[] of T
+		@list.each{|v|r.concat(v)}
+		r
+	end
 	def [](i : Tuple(Int32, Int32));@list[i[0]][i[1]];end
 	def []=(i : Tuple(Int32, Int32), x : T);@list[i[0]][i[1]] = x;end
 	def lower_bound(x : T)
