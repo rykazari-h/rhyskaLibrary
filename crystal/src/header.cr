ff0083b251d5593caee8399ba8a10441ae8cce0c
rykazari <ryota.books@outlook.jp>
2025-10-20 18:47:50 +0900

    headerを対応

--- crystal/src/header.cr
+++ crystal/src/header.cr
@@ -2,10 +2,6 @@ macro pt(s);pointerof({{s}});end
 struct Bool;def to_i;self ? 1 : 0;end;end
 macro vgen(*v);Array.new({{v[0]}}){ {% if v.size==2 %}{{v[1]}}{% else %}vgen({{*v[1..]}}){% end %} };end
 macro rvgen(type,*v);{% if v.size==2 %}Array({{type}}).new({{v[0]}}){{{v[1]}}}{% else %}Array.new({{v[0]}}){rvgen({{type}},{{*v[1..]}})}{% end %};end
-def putv(a : Array(T)) forall T;return if a.empty?;print a[0];(1...a.size).each{|i|putv " ";putv a[i]};end
-def putv(a : Array(Array(T))) forall T;return if a.empty?;putv a[0];(1...a.size).each{|i|puts;putv a[i]};end
-def putv(a : Tuple(*T)) forall T;return if a.empty?;putv a[0];(1...a.size).each{|i|putv " ";putv a[i]};end
-def putv(a);print a;end
 def max(a,b);a<b ? b : a;end
 def min(a,b);a<b ? a : b;end
 def max(*a);x=a[0];(1...a.size).each{|i|x=max(x,a[i])};x;end
@@ -14,3 +10,4 @@ macro chmin(a,b);({{a}}>{{b}} ? ({{a}}={{b}};true) : false);end
 macro chmax(a,b);({{a}}<{{b}} ? ({{a}}={{b}};true) : false);end
 macro swap(a,b);{{a}},{{b}}={{b}},{{a}};end
 def assert(cond : Bool, msg = "Assertion failed");raise msg unless cond;end
+require "fastio.cr"
