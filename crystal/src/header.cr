37a299bd9edbbc5c9a0021712a6f2c82184bbb0f
rykazari <ryota.books@outlook.jp>
2025-11-11 00:20:46 +0900

    splatを適切に書き換え

--- crystal/src/header.cr
+++ crystal/src/header.cr
@@ -1,7 +1,7 @@
 macro pt(s);pointerof({{s}});end
 struct Bool;def to_i;self ? 1 : 0;end;end
-macro vgen(*v);Array.new({{v[0]}}){ {% if v.size==2 %}{{v[1]}}{% else %}vgen({{*v[1..]}}){% end %} };end
-macro rvgen(type,*v);{% if v.size==2 %}Array({{type}}).new({{v[0]}}){{{v[1]}}}{% else %}Array.new({{v[0]}}){rvgen({{type}},{{*v[1..]}})}{% end %};end
+macro vgen(*v);Array.new({{v[0]}}){ {% if v.size==2 %}{{v[1]}}{% else %}vgen({{v[1..].splat}}){% end %} };end
+macro rvgen(type,*v);{% if v.size==2 %}Array({{type}}).new({{v[0]}}){{{v[1]}}}{% else %}Array.new({{v[0]}}){rvgen({{type}},{{v[1..].splat}})}{% end %};end
 def max(a,b);a<b ? b : a;end
 def min(a,b);a<b ? a : b;end
 def max(*a);x=a[0];(1...a.size).each{|i|x=max(x,a[i])};x;end
