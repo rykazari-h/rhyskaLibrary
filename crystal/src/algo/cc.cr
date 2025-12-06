b95a182760ccede2c3ce483c2a3b04fb42542bdd
rykazari <ryota.books@outlook.jp>
2025-12-06 18:18:46 +0900

    cc.cr

---
+++ crystal/src/algo/cc.cr
@@ -0,0 +1,24 @@
+struct CC(T)
+  property d : Array(T)
+  property did : Bool
+  private def init : Nil
+    @d = d.uniq.sort
+    @did = true
+  end
+  def initialize(@d : Array(T) = Array(T).new, @did : Bool = false); end
+  def clear; did = false; d.clear; end
+  def push(x : T); d << x; did = false; end
+  def push(z : Array(T)); z.each { |x| push x }; end
+  def call(x : T)
+    init unless did
+    d.bsearch_index { |v| x <= v } .not_nil!
+  end
+  def [](x : Int)
+    init unless did
+    d[x]
+  end
+  def size
+    init unless did
+    d.size
+  end
+end
