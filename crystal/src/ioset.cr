deb5db8c0934684582c41528ecb0f381ac767059
rykazari <ryota.books@outlook.jp>
2025-12-21 11:37:29 +0900

    豪華に

--- crystal/src/ioset.cr
+++ crystal/src/ioset.cr
@@ -4,6 +4,7 @@ class IOset
 	def fill;@size = STDIN.read @buf;@idx = 0;end
 	def read_byte : UInt8?;fill if @idx >= @size;return nil if @size == 0;b = @buf[@idx];@idx += 1;b;end
 	def trim;loop do;fill if @idx >= @size;return if @size == 0;b = @buf[@idx];if b<=32;@idx += 1;else return;end;end;end
+	def getc : Char;c = read_byte;return 0.chr if !c;while c && c <= 32;c = read_byte;end;c ? c.not_nil!.chr : 0.chr;end
 	def read_line : String;c = read_byte;return "" if !c;s = String.build do |i|;while c && c != 10;i.write_byte c;c = read_byte;end;end;s;end
 	def gets : String;c = read_byte;return "" if !c;while c && c <= 32;c = read_byte;return "" if !c;end;s = String.build do |i|;while c && c > 32;i.write_byte c;c = read_byte;end;end;s;end
 	macro geti_g(name, type)
@@ -25,11 +26,12 @@ class IOset
 			n * sign
 		end
 	end
-	geti_g(geti, Int32);geti_g(geti64, Int64)
+	geti_g(geti, Int32);geti_g(geti64, Int64);geti_g(getu, UInt32);geti_g(getu64,UInt64)
 	# ---output---
 	def write_byte(b : UInt8);flush if @oidx == BUFS;@obuf[@oidx] = b;@oidx += 1;end
 	def write(s : String);s.each_byte { |b| write_byte b };end
 	def write_int(x : Int);i = 0;n = x < 0 ? -x : x;loop do;@stk[i] = (n % 10).to_u8;i += 1;break if (n //= 10) <= 0;end;write_byte 45 if x < 0;while i > 0;write_byte @stk[i-=1] | 48;end;end
+	def write_int(n : UInt8 | UInt16 | UInt32 | UInt64 | UInt128);i = 0;loop do;@stk[i] = (n % 10).to_u8;i += 1;break if (n //= 10) <= 0;end;while i > 0;write_byte @stk[i-=1] | 48;end;end
 	def setprecision(x);@precision = x;end
 	def write_float(x : Float);v = x.to_i64;f = ((x - v).abs * 10i64 ** @precision).round.to_i64;write_int v;write_byte 46;i = 0;loop do;@stk[i] = (f % 10).to_u8;i += 1;break if (f //= 10) <= 0;end;(@precision - i).times { write_byte 48};while i > 0;write_byte @stk[i-=1] | 48;end;end
 	def flush;return if @oidx == 0;STDOUT.write @obuf[0, @oidx];@oidx = 0;end
