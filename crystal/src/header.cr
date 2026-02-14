874c6f6848ad0032881dc0f74eed660a7fd15686
rykazari <ryota.books@outlook.jp>
2026-02-14 23:28:10 +0900

    header time.diff

--- crystal/src/header.cr
+++ crystal/src/header.cr
@@ -1,4 +1,5 @@
 macro pt(s);pointerof({{s}});end
+struct Time;def diff(other : Time);(self-other).abs.total_milliseconds;end;end
 struct Bool;def to_i;self ? 1 : 0;end;end
 macro vgen(*v);Array.new({{v[0]}}){ {% if v.size==2 %}{{v[1]}}{% else %}vgen({{v[1..].splat}}){% end %} };end
 macro rvgen(type,*v);{% if v.size==2 %}Array({{type}}).new({{v[0]}}){{{v[1]}}}{% else %}Array.new({{v[0]}}){rvgen({{type}},{{v[1..].splat}})}{% end %};end
@@ -6,8 +7,8 @@ def max(a,b);a<b ? b : a;end
 def min(a,b);a<b ? a : b;end
 def max(*a);x=a[0];(1...a.size).each{|i|x=max(x,a[i])};x;end
 def min(*a);x=a[0];(1...a.size).each{|i|x=min(x,a[i])};x;end
-macro chmin(a,b);({{a}}>{{b}} ? ({{a}}={{b}};true) : false);end
-macro chmax(a,b);({{a}}<{{b}} ? ({{a}}={{b}};true) : false);end
+macro chmin(a,b);(%t={{b}};{{a}}>%t ? ({{a}}=%t;true) : false);end
+macro chmax(a,b);(%t={{b}};{{a}}<%t ? ({{a}}=%t;true) : false);end
 macro swap(a,b);{{a}},{{b}}={{b}},{{a}};end
 def assert(cond : Bool, msg = "Assertion failed");raise msg unless cond;end
 macro for(f, m, s);begin
