be252a66523b25080b70df7e51d39191b7e501b6
rykazari <ryota.books@outlook.jp>
2026-03-01 10:35:43 +0900

    sizeを高速化

--- crystal/src/data/dsu.cr
+++ crystal/src/data/dsu.cr
@@ -1,8 +1,9 @@
 class Dsu
-  def initialize(@n : Int32);@p = Array(Int32).new(@n, -1);end
+  def initialize(@n : Int32);@p = Array(Int32).new(@n, -1); @sz = @n;end
   def merge(a : Int32, b : Int32)
     a = root(a);b = root(b)
     return false if a == b
+    @sz -= 1
     if @p[a] < @p[b]
       @p[a] += @p[b];@p[b] = a
     else
@@ -13,7 +14,7 @@ class Dsu
   def same(a : Int32, b : Int32);root(a) == root(b);end
   def root(a : Int32);return a if @p[a] < 0;@p[a] = root(@p[a]);end
   def size(z : Int32);-@p[root(z)];end
-  def size;count = Array(Int32).new(@n, 0);@n.times { |i| count[root(i)] = 1 };count.sum;end
+  def size;@sz;end
   def groups
     res = Array(Array(Int32)).new(@n) { [] of Int32 };@n.times { |i| res[root(i)] << i }
     res.reject { |v| v.empty? }
