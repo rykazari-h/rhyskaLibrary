20764ac2a8c8dccdb43885a847cd818f53b5d2cc
rykazari <ryota.books@outlook.jp>
2026-06-12 23:46:04 +0900

    dynamicmodint 速く

--- crystal/src/data/dynamicmodint.cr
+++ crystal/src/data/dynamicmodint.cr
@@ -1,35 +1,55 @@
 require "math/math_ext.cr"
 struct DynamicModint
-	property mod
-	getter v
-	def initialize(@v : Int64 = 0i64, @mod = 1i64); @v %= @mod; end
+	property mod : Int64
+	getter v : Int64
+	@mod = 1i64
+	def initialize(@v : Int64 = 0i64, @mod, reduce = true); @v %= @mod if reduce; end
 	def self.zero(m : Int64 = 1i64)
-		new 0i64, m
+		new 0i64, m, false
 	end
 	def val; @v; end
 	def to_i; @v; end
 	def ==(other : self)
 		@v == other.v
 	end
+	def -
+			self.class.new @v == 0 ? 0i64 : @mod - @v, @mod, false
+	end
 	def +(other : self)
-		self.class.new (@v + other.v) % @mod, @mod
+		v = @v + other.v
+		v -= @mod if @mod <= v
+		self.class.new v, @mod, false
 	end
 	def -(other : self)
-		self.class.new (@v - other.v) % @mod, @mod
+		v = @v - other.v
+		v += @mod if v < 0
+		self.class.new v, @mod, false
 	end
 	def *(other : self)
-		self.class.new (@v.to_i128 * other.v % @mod).to_i64, @mod
+		self.class.new (@v.to_i128 * other.v % @mod).to_i64, @mod, false
 	end
 	def /(other : self)
-		self.class.new @v.to_i128 * modinv(other.v, @mod) % @mod, @mod
+		self.class.new @v.to_i128 * modinv(other.v, @mod) % @mod, @mod, false
 	end
-	def +(other : Int); self + self.class.new(other, @mod); end
-	def -(other : Int); self - self.class.new(other, @mod); end
-	def *(other : Int); self * self.class.new(other, @mod); end
-	def /(other : Int); self / self.class.new(other, @mod); end
+	def +(other : Int)
+		v = @v + other % @mod
+		v -= @mod if @mod <= v
+		self.class.new v, @mod, false
+	end
+	def -(other : Int)
+		v = @v - other % @mod
+		v += @mod if v < 0
+		self.class.new v, @mod, false
+	end
+	def *(other : Int)
+		self.class.new (@v.to_i128 * (other % @mod) % @mod).to_i64, @mod, false
+	end
+	def /(other : Int)
+		self.class.new (@v.to_i128 * modinv(other % @mod, @mod) % @mod).to_i64, @mod, false
+	end
 	def ==(other : Int); @v == other; end
 	def pow(k)
-		res = self.class.new 1, @mod
+		res = self.class.new 1, @mod, false
 		a = self
 		while k != 0
 			res *= a if k & 1 != 0
@@ -39,7 +59,7 @@ struct DynamicModint
 		res
 	end
 	def inv
-		self.class.new modinv @v, @mod
+		self.class.new modinv(@v, @mod), @mod, false
 	end
 end
 class IOset
